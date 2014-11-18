6c812b460f06b172f191da9b82738fb6243caa93
Graham Voysey <gvoysey@bu.edu>
2014-11-18 17:55:09 -0500

    updated to include XML parsing and stubbed in support for background colors.

---
+++ WidgetView/TodayViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TodayViewController.swift
+//  WidgetView
+//
+//  Created by Graham Voysey on 11/18/14.
+//  Copyright (c) 2014 Graham Voysey. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+
+class TodayViewController: UIViewController, NCWidgetProviding, NSXMLParserDelegate {
+    
+    
+    @IBOutlet var frenchToastLabel : UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        self.updateFrenchToastLevel()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    func updateFrenchToastLevel() {
+        var url:NSURL = NSURL(string:"http://www.universalhub.com/toast.xml")!
+        var error: NSError?
+        var data:NSData = NSData(contentsOfURL: url)!
+        
+        if let xmlDoc = AEXMLDocument(xmlData: data, error: &error){
+            var level = xmlDoc.rootElement["frenchtoast"]["status"].value
+            switch level{
+                case "Low":
+                    self.frenchToastLabel.backgroundColor = UIColor(red: 151, green:255, blue:155, alpha:0.9)
+                case "Guarded"
+                    self.frenchToastLabel.backgroundColor = UIColor(red: 151, green:153, blue: 255, alpha:0.9)
+                case "Elevated"
+                    self.frenchToastLabel.backgroundColor = UIColor(red: 255, green:255, blue: 64, alpha:0.9)
+                case "High"
+                    self.frenchToastLabel.backgroundColor = UIColor(red: 255, green:130, blue: 29, alpha:0.9)
+                case "Severe"
+                    self.frenchToastLabel.backgroundColor = UIColor(red: 248, green:93, blue: 88, alpha:0.9)
+            }
+            self.frenchToastLabel.text = level
+        }
+        else{
+            self.frenchToastLabel.text = "Loading ..."
+        }
+    }
+}
+
+// from http://stackoverflow.com/questions/24074257/how-to-use-uicolorfromrgb-value-in-swift/25276178#25276178
+extension UIColor {
+        convenience init(rgb: UInt) {
+            self.init(
+                red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgb & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0)
+            )
+ }
