e8fa13bce621ceec2249af900337ff275b9e5f27
Graham Voysey <gvoysey@bu.edu>
2014-11-24 22:30:55 -0500

    updated colors to match uhub color scheme exactly.

--- WidgetView/TodayViewController.swift
+++ WidgetView/TodayViewController.swift
@@ -48,15 +48,15 @@ class TodayViewController: UIViewController, NCWidgetProviding, NSXMLParserDeleg
             var level = xmlDoc.rootElement["status"].value
             switch level{
                 case "Low":
-                    self.frenchToastLabel.backgroundColor = UIColor(red: 151, green:255, blue:155, alpha:0.9)
+                    self.frenchToastLabel.backgroundColor = UIColor(rgba: "#97ff9b")
                 case "Guarded":
-                    self.frenchToastLabel.backgroundColor = UIColor(red: 151, green:153, blue: 255, alpha:0.9)
+                    self.frenchToastLabel.backgroundColor = UIColor(rgba: "#9799ff")
                 case "Elevated":
-                    self.frenchToastLabel.backgroundColor = UIColor(red: 255, green:255, blue: 64, alpha:0.9)
+                    self.frenchToastLabel.backgroundColor = UIColor(rgba: "#ffff40")
                 case "High":
-                    self.frenchToastLabel.backgroundColor = UIColor(red: 255, green:130, blue: 29, alpha:0.9)
+                    self.frenchToastLabel.backgroundColor = UIColor(rgba: "#ff821d")
                 case "Severe":
-                    self.frenchToastLabel.backgroundColor = UIColor(red: 248, green:93, blue: 88, alpha:0.9)
+                    self.frenchToastLabel.backgroundColor = UIColor(rgba: "#f85d58")
                 default:
                     break
             }
@@ -66,14 +66,36 @@ class TodayViewController: UIViewController, NCWidgetProviding, NSXMLParserDeleg
     }
 }
 
-// from http://stackoverflow.com/questions/24074257/how-to-use-uicolorfromrgb-value-in-swift/25276178#25276178
 extension UIColor {
-        convenience init(rgb: UInt) {
-            self.init(
-                red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgb & 0x0000FF) / 255.0,
-                alpha: CGFloat(1.0)
-            )
+    convenience init(rgba: String) {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        if rgba.hasPrefix("#") {
+            let index = advance(rgba.startIndex, 1)
+            let hex = rgba.substringFromIndex(index)
+            let scanner = NSScanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexLongLong(&hexValue) {
+                if countElements(hex) == 6 {
+                    red = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
+                    blue = CGFloat(hexValue & 0x0000FF) / 255.0
+                } else if countElements(hex) == 8 {
+                    red = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF) / 255.0
+                } else {
+                    print("invalid rgb string, length should be 7 or 9")
+                }
+            } else {
+                println("scan hex error")
+            }
+        } else {
+            print("invalid rgb string, missing '#' as prefix")
         }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
 }
